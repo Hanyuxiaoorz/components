942ea8a5cb45166b6d79099934b1034806925867
hanyuxiao <hanyuxiao@cmiot.chinamobile.com>
2020-05-25 15:29:05 +0800

    feat: docker 启动脚本

---
+++ docker/build.sh
@@ -0,0 +1,11 @@
+# 清理 Docker container
+docker rm -f CONTAINER_NAME
+
+# 清理 Docker images
+docker rmi IMAGE_NAME
+
+# 通过文件构建 Image
+docker build -t IMAGE_NAME .
+
+# 启动 Image container
+docker run --name CONTAINER_NAME -p HOST_PORT:CONTAINER_PORT IMAGE_NAME
