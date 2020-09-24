99ac3b44bf1815cbe8c0206f9c264b21fbf92192
hanyuxiao <hanyuxiao@cmiot.chinamobile.com>
2020-09-24 10:19:14 +0800

    refactor: 重构创建数据库命令

---
+++ mysql/database.sql
@@ -0,0 +1,3 @@
+-- 创建数据
+drop database if exists `db_name`;
+create database `db_name` default character set utf8 collate utf8_general_ci;
--- mysql/mysql.sql
+++
@@ -1,3 +0,0 @@
--- 创建数据
-drop database if exists `jarvis`;
-create database `jarvis` default character set utf8 collate utf8_general_ci;
