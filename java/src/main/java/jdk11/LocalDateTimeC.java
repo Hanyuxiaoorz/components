ac2b6a9731b212446bae7cdd382d34b08d208a07
hanyuxiao <hanyuxiao@cmiot.chinamobile.com>
2020-09-24 10:15:34 +0800

    feat: LocalDateTime 与 Date 的相互转换

---
+++ java/src/main/java/jdk11/LocalDateTimeC.java
@@ -0,0 +1,29 @@
+package jdk11;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * {@link java.time.LocalDateTime}
+ */
+public class LocalDateTimeC {
+
+    /**
+     * 把 {@link LocalDateTime} 转换为 {@link java.util.Date}
+     */
+    public void toDate(LocalDateTime localDateTime) {
+        Date date = Date.from(
+                localDateTime
+                        .atZone(ZoneId.systemDefault())
+                        .toInstant()
+        );
+    }
+
+    /**
+     * 把 {@link Date} 转换为 {@link LocalDateTime}
+     */
+    public void of(Date date) {
+        LocalDateTime localDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+}
