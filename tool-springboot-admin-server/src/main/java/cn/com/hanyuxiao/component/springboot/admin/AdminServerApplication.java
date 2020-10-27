1f178e430e441253513e8bdc797983796d501c79
hanyuxiao <hanyuxiao@cmiot.chinamobile.com>
2020-10-27 15:10:12 +0800

    feat: springbootadmin 重命名为 springboot-admin

---
+++ tool-springboot-admin-server/src/main/java/cn/com/hanyuxiao/component/springboot/admin/AdminServerApplication.java
@@ -0,0 +1,27 @@
+package cn.com.hanyuxiao.component.springboot.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+}
