51275eee2b281e4fc1e1caa0f4bb97beeb88ad45
hanyuxiao <hanyuxiao@cmiot.chinamobile.com>
2020-10-27 14:16:08 +0800

    feat: SpringBoot 框架

---
+++ framework-springboot/src/test/java/cn/com/hanyuxiao/component/springboot/ApplicationTest.java
@@ -0,0 +1,19 @@
+package cn.com.hanyuxiao.component.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@SpringBootApplication
+@RunWith(SpringRunner.class)
+public class ApplicationTest {
+
+    @Test
+    public void run() {
+        
+    }
+
+}
