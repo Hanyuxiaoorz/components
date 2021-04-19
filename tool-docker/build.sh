3934909901c360d65e8caf131a9d686cdd4d145d
hanyuxiao <hanorz@foxmail.com>
2021-04-19 15:36:50 +0800

    feat: Docker 容器构建脚本加入参数

--- tool-docker/build.sh
+++ tool-docker/build.sh
@@ -1,11 +1,14 @@
+IMAGE_NAME=xxx
+CONTAINER_NAME=xxx
+
 # 清理 Docker container
-docker rm -f CONTAINER_NAME
+docker rm -f $CONTAINER_NAME
 
 # 清理 Docker images
-docker rmi IMAGE_NAME
+docker rmi $IMAGE_NAME
 
 # 通过文件构建 Image
-docker build -t IMAGE_NAME .
+docker build -t $IMAGE_NAME .
 
 # 启动 Image container
-docker run --name CONTAINER_NAME -p HOST_PORT:CONTAINER_PORT --restart=always -d IMAGE_NAME
+docker run --name $CONTAINER_NAME -p HOST_PORT:CONTAINER_PORT --restart=always -d IMAGE_NAME
