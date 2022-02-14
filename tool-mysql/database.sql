b28e063c9e16a98683ea5c35066c43c8b334d9a0
hanyuxiao <hanorz@foxmail.com>
2022-02-14 14:27:00 +0800

    delete: 移除之前所有的仓库

--- tool-mysql/database.sql
+++
@@ -1,3 +0,0 @@
--- 创建数据
-drop database if exists `db_name`;
-CREATE DATABASE `db_name` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- tool-mysql/table.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists `TABLE_NAME`;
-create table `TABLE_NAME` (
-                           `F_NAME` integer primary key comment '注释'
-)engine=InnoDB default charset=utf8mb4 comment '表注释';
-
-create index `TABLE_INDEX_NAME` on account(F_NAME);
