e8b6f98c5f3edf813afb202ebfff1da8cc08bd48
hanyuxiao <hanorz@foxmail.com>
2022-02-14 14:52:54 +0800

    feat: SpringBoot Docker 的支持

---
+++ SpringBoot-Docker/src/main/java/cn/vxnm/component/springboot/docker/SpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package cn.vxnm.component.springboot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+}
---
+++ SpringBoot-Docker/src/test/java/cn/vxnm/component/springboot/docker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.vxnm.component.springboot.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
