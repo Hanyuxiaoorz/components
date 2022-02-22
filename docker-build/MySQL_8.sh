4898d3535caa8d20de42231e6f5f80b60ea4f17c
hanyuxiao <hanorz@foxmail.com>
2022-02-22 11:37:04 +0800

    feat: MySQL_8.sh 配置说明

--- docker-build/MySQL_8.sh
+++ docker-build/MySQL_8.sh
@@ -1,3 +1,11 @@
+# --name => 容器名称
+# -p => 宿主机端口 : 内部容器端口
+# -e MYSQL_ROOT_PASSWORD => 数据库 root 密码
+# --character-set => 数据库默认字符集
+# --collation-server 数据库默认排序字符集
+# -v 宿主机存储路径 : 内部容器存储路径 / /var/lib/mysql 为 MySQL 数据存储路径
+# --restart=always => 服务自动重启
+
 docker run -d --name mysql_8 \
   -p 3306:3306 \
   -p 33060:33060 \
