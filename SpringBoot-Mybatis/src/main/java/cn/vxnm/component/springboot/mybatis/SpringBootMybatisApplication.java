b7877438bdb55736d89433ab1397d3cf8300b53a
hanyuxiao <zapwe@qq.om>
2022-06-12 15:09:10 +0800

    feat: SpringBoot Mybatis init

---
+++ SpringBoot-Mybatis/src/main/java/cn/vxnm/component/springboot/mybatis/SpringBootMybatisApplication.java
@@ -0,0 +1,13 @@
+package cn.vxnm.component.springboot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Mybatis/src/test/java/cn/vxnm/component/springboot/mybatis/SpringBootMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.vxnm.component.springboot.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
