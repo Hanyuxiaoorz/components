db34043952de2068bcade1529660e753e74fdcbe
hanyuxiao <zapwe@qq.om>
2022-06-13 16:13:01 +0800

    feat: SpringBoot Mybatis maven dependency and framework config

---
+++ SpringBoot-Mybatis/src/main/java/cn/vxnm/component/springboot/mybatis/mapper/DemoMapper.java
@@ -0,0 +1,10 @@
+package cn.vxnm.component.springboot.mybatis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface DemoMapper {
+
+}
